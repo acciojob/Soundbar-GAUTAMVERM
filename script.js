ee3e93c55087da38add4fde93379672a3a99469d
GAUTAMVERM <89487056+GAUTAMVERM@users.noreply.github.com>
2025-04-27 00:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+  const btn = document.getElementById(sound);
+
+  btn.addEventListener('click', () => {
+    stopSounds();
+    const audio = new Audio(`sounds/${sound}.mp3`);
+    audio.play();
+    window.currentAudio = audio;
+  });
+});
+
+document.getElementById('stop').addEventListener('click', stopSounds);
+
+function stopSounds() {
+  if (window.currentAudio) {
+    window.currentAudio.pause();
+    window.currentAudio.currentTime = 0;
+  }
+}
